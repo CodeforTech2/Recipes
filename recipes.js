f311d68ffa5dc1580699627d79b91eb5b7241ed4
CodeforTech <stoineaalin2501@gmail.com>
2024-07-06 19:40:18 +0300

    update

--- recipes.js
+++ recipes.js
@@ -1,2 +1,8 @@
 // - pricing page
 // - cart and logo symbol
+const top = document.getElementById('top');
+top.onclick = function() {
+    window.scrollTo({top: 0});
+}
+
+top.onclick = () => window.scrollTo({top: 0});
