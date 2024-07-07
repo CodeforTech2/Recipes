f207811aeb73c2d0d8f97e97b079e08e2d3e113c
CodeforTech <stoineaalin2501@gmail.com>
2024-07-07 17:39:05 +0300

    update

--- recipes.js
+++ recipes.js
@@ -10,3 +10,10 @@ reset.onclick = function() {
 // top.onclick = () => window.scrollTo({top: 0});
 
 // start FreeCodeCamp projects part time
+
+let first = 2;
+first++;
+
+console.log(first);
+
+console.log((4 + 6 + 9) / 77);
