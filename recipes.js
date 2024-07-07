504fd8ad68575de9c1db17e1cbe597d2ded84388
CodeforTech <stoineaalin2501@gmail.com>
2024-07-07 19:18:26 +0300

    update

--- recipes.js
+++ recipes.js
@@ -11,7 +11,39 @@ reset.onclick = function() {
 
 // start FreeCodeCamp projects part time
 
-const string = "Helo, ";
-const str2 = "John!";
+const str = "Helo, ";
+const str2 = `John!`;
 
-console.log(str + str2);
+// console.log(str + str2);
+
+const mes = `${str}${str2}`;
+console.log(mes);
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+console.log(text.charAt(text.length-1))
+
+console.log(text.at(-2))
+console.log(text.slice(5, 10));
+console.log(text.toLocaleLowerCase());
+console.log(str.concat(str2));
+
+// alert (3 > 5);
+// alert ("test" == "test");
+console.log("test" != "test");
+console.log(2 > 3);
+
+let num1 = 3;
+let num2 = 5;
+
+if (num1 > num2) {
+    console.log("That's correct!");
+} else {
+    console.log("That's wrong!");
+}
+
+if (num1 > 2 || num2 > 10) {
+    console.log( "Logical si True!");
+} else {
+    console.log("Logical si False!");
+}
