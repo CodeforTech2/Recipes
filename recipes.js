c432e030a129824468bb14a82a099da484cfac4f
CodeforTech <stoineaalin2501@gmail.com>
2024-07-28 12:15:48 +0300

    js learning

--- recipes.js
+++ recipes.js
@@ -11,40 +11,6 @@ reset.onclick = function() {
 
 // start FreeCodeCamp projects part time
 
-const str = "Helo, ";
-const str2 = `John!`;
-
-// console.log(str + str2);
-
-const mes = `${str}${str2}`;
-console.log(mes);
-
-let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-console.log(text.charAt(text.length-1))
-
-console.log(text.at(-2))
-console.log(text.slice(5, 10));
-console.log(text.toLocaleLowerCase());
-console.log(str.concat(str2));
-
-// alert (3 > 5);
-// alert ("test" == "test");
-
-let hour = 12;
-let minute = 30;
-
-if ( hour == 12 && minute == 30) {
-    console.log("Este ora 12:30");
-};
-
-
-function favoriteAnimal(animal = 'Rex') {
-    // return animal + " is my favorite animal!";
-    console.log(` ${animal} is my favorite animal!`);
-}
-
-favoriteAnimal("Labus");
 // console.log(favoriteAnimal('Labus'));
 
 let exemple = "aU IESIT niste BIscUITI buni"
@@ -59,4 +25,22 @@ function lastLetter(str) {
     return str.charAt(str.length - 1);
 }
 
-console.log(lastLetter(exemple))
+// console.log(lastLetter(exemple))
+
+// ==============================================================
+// let num = prompt(parseInt("Put a number!"))
+function game(num) {
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 == 0 && i % 5 === 0) {
+            console.log("FizzBuzz!");
+        } else if (i % 3 === 0) {
+            console.log("Fizz!");
+        } else if (i % 5 === 0) {
+            console.log("Buzz!");
+        }  else {
+            console.log(i);
+        }
+    }
+}
+
+// game(5);
